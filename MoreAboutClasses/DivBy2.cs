7dbfcd08c2bc0f6e43693e1e9ccdfe23bdf24ecf
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-02 15:38:03 -0700

    Add project files.

---
+++ MoreAboutClasses/DivBy2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreAboutClasses
+{
+    public class DivBy2
+    {
+        public void Divide(int num)
+        {
+            Fancy.Write(num / 2 + "\n\n", afterPause: 1200);
+        }
+
+        public void Divide(int num, out int divisions)
+        {
+            divisions = 0;
+            for (; num / 2 >= 1; )
+            {
+                num /= 2;
+                divisions++;
+            }
+        }
+    }
+}
---
+++ MoreAboutClasses/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MoreAboutClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fancy.Write("This program uses classes to ","divide"," a number by 2.\n", pause: 500, afterPause: 500);
+
+            DivBy2 div = new DivBy2();
+
+            bool loop = true;
+            while (loop)
+            {
+                // Get the number to be divided by 2
+                int number = 0;
+                bool badNumber = true;
+                while (badNumber)
+                {
+                    Fancy.Write("Enter an integer:  ");
+                    try
+                    {
+                        number = Convert.ToInt32(Console.ReadLine());
+                        badNumber = false;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Enter an INTEGER, please...\n");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unknown exception: {0}", e.GetType());
+                    }
+                }
+
+                Fancy.Write(String.Format("\n\tYour number divided by Two: {0} / 2 = ", number));
+                div.Divide(number);
+
+                Fancy.Write("This is how many times we could divide " + number + " Before we are less than 2: ");
+
+                // Getting the out parameter
+                int divisions = 0;
+                div.Divide(number, out divisions);
+
+                Fancy.Write(divisions + "\n", 100, afterPause: 250);
+
+                // Go again?
+                Fancy.Write("\nDo you want to enter a new number? (y or n) ", 5);
+                string loopChoice = Console.ReadLine();
+                if (loopChoice != "y")
+                {
+                    loop = false;
+                }
+            }
+        }
+    }
+}
